daa8e2c3df816bf6d5ad91af15fe3c6d2fe8542c
Okan Bakırcı <132247985+bkrcokan13@users.noreply.github.com>
2023-10-17 15:06:36 +0300

    Fixing banner.py
    Product collector is fixed, renew all CSS  classes

--- banner.py
+++ banner.py
@@ -1,41 +1,44 @@
+import datetime
+import json
+
 import requests, time
 from bs4 import BeautifulSoup
+from urllib.parse import unquote
+
+
 class Banner:
     def __init__(self):
         self._headers = {
-             'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+            'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                            'AppleWebKit/537.36 (KHTML, like Gecko)'
                            'Chrome/116.0.0.0 Safari/537.36'),
             'Accept-Language': 'en-US, en;q=0.5'
         }
-        
+
         self._base_url = "https://www.gumtree.com"
         self._search_url = "https://www.gumtree.com/search?search_category=all&q="
 
-       
         self._categories = {}
 
         self._advertlist = {
-            'for-sale':[],
-            'flats-houses':[],
-            'jobs':[],
-            'business-services':[],
-            'community':[],
-            'pets':[]
+            'for-sale': [],
+            'flats-houses': [],
+            'jobs': [],
+            'business-services': [],
+            'community': [],
+            'pets': []
         }
-    
+
     def url_collect(self):
 
         data = None
-
-
 
         try:
             request_url = requests.get(self._base_url, headers=self._headers, timeout=1)
             print("Sending request waiting...")
             time.sleep(1.2)
-            
-            #Check status code
+
+            # Check status code
             if request_url.status_code == 200:
                 print("Status: Ok !")
                 data = request_url.content
@@ -45,29 +48,28 @@ class Banner:
                 print("Status: Service Temporarily Unavailable ! (NGINX)")
             else:
                 print("Status: Request is failed, check url and retry ! ")
-            
 
             # Get Categories Url
 
             url_soup = BeautifulSoup(data, 'html.parser')
 
-            container =  url_soup.find_all(
-                'ul',attrs={
-                    'class':'css-wwpisx e4qfz7d2'
+            container = url_soup.find_all(
+                'ul', attrs={
+                    'class': 'css-wwpisx e4qfz7d2'
                 }
             )
 
             for containerData in container:
                 liData = containerData.find_all(
                     'li', attrs={
-                        'class':'css-14yx5j e1c1vrhj2'
+                        'class': 'css-14yx5j e1c1vrhj2'
                     }
                 )
 
                 for li in liData:
                     aData = li.find_all(
-                        'a',attrs={
-                            'class':'css-8z8t0 e1c1vrhj0'
+                        'a', attrs={
+                            'class': 'css-8z8t0 e1c1vrhj0'
                         }
                     )
 
@@ -80,35 +82,36 @@ class Banner:
                             pass
                         else:
                             self._categories[str(urlName[3])] = str(self._base_url + a.get("href"))
-        
+
             print("Status: Url collected !")
-            print(self._categories)
-           
+            # print(self._categories)
+
             self.collect_products()
         except Exception as exception:
             print(exception)
-    
+
     def collect_products(self):
         try:
-           
-            for urls in list(self._categories.keys()):
-               self.find_products(categories_url=self._categories[urls],categories_name=urls)
 
-            print(self._advertlist)
+            for urls in list(self._categories.keys()):
+                self.find_products(categories_url=self._categories[urls], categories_name=urls)
+
+            # print(self._advertlist)
+            self.extract_data()
         except Exception as exception:
             print("Exception : Collect Products !")
             print(exception)
-    
-    #/uk/page2
+
+    # /uk/page2
     def find_products(self, categories_url, categories_name):
         try:
 
             print(f"CURRENT CATEGORIES : {categories_name}")
             # Page count max 10 page
-            for pageCount in range(1, (2+1),1):
+            for pageCount in range(1, (2 + 1), 1):
 
                 # Request advert page
-                try: 
+                try:
 
                     addedUrl = categories_url + f"/uk/page{pageCount}"
 
@@ -128,44 +131,59 @@ class Banner:
                         print("Status : Service Unavailable ! (NGINX)")
                 except requests.RequestException as reqExp:
                     print(reqExp)
-                
+
                 # Parse advert page
                 productSoup = BeautifulSoup(productRequest.content, 'html.parser')
 
                 productsDiv = productSoup.find_all(
                     'div', attrs={
-                        'class':'css-pu9hbu e1j2ibpb1'
+                        'class': 'css-1a67vzc e1j2ibpb0'
                     }
                 )
 
                 for divs in productsDiv:
                     hrefs = divs.find_all(
                         'a', attrs={
-                            'class':'css-220ynt e1l3kmil19'
+                            'class': 'css-220ynt e1l3kmil19'
                         }
                     )
-                    
+
                     for href in hrefs:
-                        self._advertlist[categories_name].append(str(self._base_url+ href.get("href")))
 
-                        # print(str(self._base_url+ href.get("href")))
+                        cleanedHref = str(href.get("href")).replace('\u00a3', '£')
+                        self._advertlist[categories_name].append(str(self._base_url + cleanedHref))
 
-
-
+                        # print(str(self._base_url + href.get("href")))
         except Exception as exp:
             print(exp)
-    
+
+    def extract_data(self):
+
+        try:
+            file_create_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            file_name = f"{file_create_time}.json"
+
+            json_data = json.dumps(self._advertlist, indent=4)
+
+            with open(file_name, 'w') as jsonFile:
+                jsonFile.write(json_data)
+
+            print(f"[{file_create_time}]:{file_name} is extracted.")
+        except Exception as jsonException:
+            print(jsonException)
+
     def advert_title(self):
         pass
 
     def advert_price(self):
         pass
-    
+
     def advert_id(self):
         pass
-    
+
     def advert_img(self):
         pass
+
 
 app = Banner()
 app.url_collect()
